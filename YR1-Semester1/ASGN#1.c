f237b1257ed6efb9baaef744fea81220c3ac24e9
Medine Crux <helios3279@gmail.com>
2024-09-05 22:37:14 +0800

    Assignment 1

---
+++ YR1-Semester1/ASGN#1.c
@@ -0,0 +1,11 @@
+#include <stdio.h> //brings up main directory
+
+int main() {
+//defines entry point of the code
+
+    printf("Hello World!");
+    //prints the text output "Hello World!"
+
+    return 0;
+    //ends the C program
+}
